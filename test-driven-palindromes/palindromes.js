6632d1ca59ee8ce19b23276f449cbeb28691eefd
Sirealism <m4popesc@ryerson.ca>
2018-12-21 17:29:16 -0500

    Palindromes only returns palindromes, non-palindromes aren't included

--- test-driven-palindromes/palindromes.js
+++ test-driven-palindromes/palindromes.js
@@ -3,5 +3,21 @@ module.exports = string => {
   const cleanString = string => {
     return string.toLowerCase().replace(/[^a-z]/g, "");
   };
-  return [cleanString(string)];
+
+  const isPalindrome = string => {
+    var stillPalindrome = true;
+    for (
+      var left = 0, right = string.length - 1;
+      stillPalindrome && left < right;
+      left++, right--
+    ) {
+      var equal = string.charAt(left) == string.charAt(right);
+      stillPalindrome = stillPalindrome && equal;
+    }
+    return stillPalindrome;
+  };
+
+  var cleaned = cleanString(string);
+  if (isPalindrome(cleaned)) return [cleaned];
+  return [];
 };
