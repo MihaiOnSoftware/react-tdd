1bee746aba3ba04590825cc351a7affe0d898b4d
Sirealism <m4popesc@ryerson.ca>
2018-12-24 00:16:27 -0500

    Refactoring, using the 'rewrite algorithm' refactor since the original algorithm couldn't really be broken down

--- test-driven-palindromes/palindromes.js
+++ test-driven-palindromes/palindromes.js
@@ -14,20 +14,38 @@ module.exports = string => {
     return isPalindromeRecursive(string, true);
   };
 
-  const findPalindromes = string => {
-    var allPalindromes = [];
-    var minEnd = 0;
+  const substrings = string => {
+    var pairs = [];
     for (var start = 0; start < string.length; start++) {
-      for (var end = string.length; end > start + 1 && end > minEnd; end--) {
-        var current = string.substr(start, end);
-        if (isPalindrome(current)) {
-          minEnd = end;
-          allPalindromes.push(current);
-        }
+      for (var end = string.length; end > start + 1; end--) {
+        pairs.push([start, end]);
       }
     }
+    return pairs;
+  };
 
-    return allPalindromes;
+  const findPalindromes = string => {
+    var allSubStrings = substrings(string);
+
+    const reducer = (accumulator, current) => {
+      var substring = string.substr(current[0], current[1]);
+      if (
+        (accumulator.length == 0 ||
+          accumulator[accumulator.length - 1][1] < current[1]) &&
+        isPalindrome(substring)
+      ) {
+        accumulator.push(current);
+      }
+      return accumulator;
+    };
+
+    var allPalindromes = allSubStrings.reduce(reducer, []);
+
+    var allPalindromeStrings = allPalindromes.map(palindrome => {
+      return string.substr(palindrome[0], palindrome[1]);
+    });
+
+    return allPalindromeStrings;
   };
 
   var cleaned = cleanString(string);
