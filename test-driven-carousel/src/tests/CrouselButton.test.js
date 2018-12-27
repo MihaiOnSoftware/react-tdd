3d3377d409fbd7cf120459ffb7023b065d8c54e7
Sirealism <m4popesc@ryerson.ca>
2018-12-27 00:35:49 -0500

    Refactoring, making the test less... intrusive

--- test-driven-carousel/src/tests/CrouselButton.test.js
+++ test-driven-carousel/src/tests/CrouselButton.test.js
@@ -8,25 +8,33 @@ configure({ adapter: new Adapter() });
 
 describe("CarouselButton", () => {
   const text = "Button text";
-  let wrapper;
-
-  beforeEach(() => {
-    wrapper = shallow(<CarouselButton>{text}</CarouselButton>);
-  });
+  const defaultWrapper = () => {
+    return shallow(<CarouselButton>{text}</CarouselButton>);
+  };
 
   it("renders a <button>", () => {
-    expect(wrapper.type()).toBe("button");
+    expect(defaultWrapper().type()).toBe("button");
   });
 
   it("passes `children` through to the <button>", () => {
-    expect(wrapper.prop("children")).toBe(text);
+    expect(defaultWrapper().prop("children")).toBe(text);
   });
 
   it("passes other props through to the <button>", () => {
     const onClick = () => {};
     const className = "my-carousel-button";
     const dataAction = "prev";
-    wrapper.setProps({ onClick, className, "data-action": dataAction });
+
+    const wrapper = shallow(
+      <CarouselButton
+        onClick={onClick}
+        className={className}
+        data-action={dataAction}
+      >
+        {text}
+      </CarouselButton>
+    );
+
     expect(wrapper.prop("onClick")).toBe(onClick);
     expect(wrapper.prop("className")).toBe(className);
     expect(wrapper.prop("data-action")).toBe(dataAction);
