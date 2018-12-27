7dca2385b0778c9bbe7500bed7250a713a1d0615
Sirealism <m4popesc@ryerson.ca>
2018-12-27 00:44:11 -0500

    Start implementing CarouselSlide

---
+++ test-driven-carousel/src/CarouselSlide.js
@@ -0,0 +1,4 @@
+// src/CarouselSlide.js
+import React from "react";
+const CarouselSlide = () => <figure />;
+export default CarouselSlide;
---
+++ test-driven-carousel/src/tests/CarouselSlide.test.js
@@ -0,0 +1,10 @@
+// src/tests/CarouselSlide.test.js
+import React from "react";
+import { shallow } from "enzyme";
+import CarouselSlide from "../CarouselSlide";
+describe("CarouselSlide", () => {
+  it("renders a <figure>", () => {
+    const wrapper = shallow(<CarouselSlide />);
+    expect(wrapper.type()).toBe("figure");
+  });
+});
